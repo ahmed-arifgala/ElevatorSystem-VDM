485e928dd765592b9d03cab11bc4a78a529f9a9a
ahmed-arifgala <126764357+ahmed-arifgala@users.noreply.github.com>
2024-01-09 14:08:30 +0500

    Driver Function Made

--- Elevator.java
+++ Elevator.java
@@ -1,6 +1,4 @@
-
-
-
+import java.util.Scanner;
 
 
 //TYPES in VDM: ElevatorSignal and DoorSignal that are used to signal the hardware components
@@ -306,8 +304,86 @@ public class Elevator implements InvariantCheck {
 
 //TESTER CLASS
 
+class ElevatorTester {
+
+    //Main Function
+      public static void main(String[] args){
+        System.out.println("Main Function");
+
+        char choice; //for storing user input of option choice
+        Scanner scanner = new Scanner(System.in);
 
 
+        try {
+
+            Elevator elevator = new Elevator(); //creating obj of the Elevator class
+
+            do{
+                System.out.println("\n\t\tElevator Sytem\n");
+                System.out.println("1: Request Elevator");
+                System.out.println("2: Request Floor");
+                System.out.println("3: Quit");
+                System.out.println("Enter Choice 1-3"); 
+                
+                choice = scanner.next().charAt(0); //reading user input
+
+                System.out.println();
+
+                try {
+                    switch (choice) {
+                        case '1':
+                            option1(elevator);
+                            break;
+                        
+                        case '2':
+                            option2(elevator);
+                            break;
+                
+                        default:
+                            break;
+                    }
+                } catch (VDMException e) { // to catch invariant and precondition violations in operations
+                    e.printStackTrace();
+                }
+
+            }while(choice != '4');
+
+        } catch (Exception e) { //to catch invariant violation in initialization of Elevator object
+            System.out.println("Initialization values violate invariant!");
+            System.out.println("\nPress Enter to quit");
+            scanner.nextLine();
+
+        }
+        
+        scanner.close();
+
+    }; 
+
+
+    public static void option1(Elevator elevator){
+ 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Current Floor Number: ");
+        int userInput = scanner.nextInt();
+        elevator.reqElevator(userInput); 
+
+        scanner.close();
+    };
+
+    public static void option2(Elevator elevator){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Destination Floor Number: ");
+        int userInput = scanner.nextInt();
+        elevator.reqFloor(userInput);
+
+        scanner.close();
+    };
+    
+
+}
 
 
 
